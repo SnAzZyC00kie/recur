d493cb8b4a927a049968323d02c52d8580ef2c99
SnAzZyC00kie <SnAzZyC00kie@users.noreply.github.com>
2019-04-28 18:10:35 -0400

    Recursive function

---
+++ digitSum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+int digitSum(int i)
+   {
+	   int sum=0;
+	   if(i==0)
+	     {
+			 return i;
+	      }
+	   else 
+	     {
+			 sum+= (i%10) + digitSum(i/10);
+	      }
+	      return sum;
+   }
+int main()
+{
+	cout << "digitSum(1364) is equal to the sum of " <<digitSum(1364) <<endl;
+	cout << "digitSum(-1364) is equal to the sum of " <<digitSum(-1364) <<endl;
+	cout << "digitSum(152) is equal to the sum of " <<digitSum(152) <<endl;
+	cout << "digitSum(33) is equal to the sum of " <<digitSum(33) <<endl;
+}
